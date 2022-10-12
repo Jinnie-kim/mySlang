099fb0df146351620309485ebc494e62357b2785
Jinnie-kim <hjk7639@gmail.com>
2022-10-12 20:43:47 +0900

    Feat: react router를 이용해 각 페이지 라우터 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
-function App() {
-  return <div></div>;
-}
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import InitPage from './pages/InitPage';
+import Login from './pages/Login';
+import Join from './pages/Join';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<InitPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/join" element={<Join />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
