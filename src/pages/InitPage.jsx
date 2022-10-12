1984856ac818117adac5303b1446a94fa1b092c6
Jinnie-kim <hjk7639@gmail.com>
2022-10-12 20:26:06 +0900

    Feat: 로그인, 회원가입 버튼 Link 태그로 페이지 이동 연결

--- src/pages/InitPage.jsx
+++ src/pages/InitPage.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import Wrapper from '../components/Wrapper';
 
 const InitPage = () => {
@@ -6,18 +7,22 @@ const InitPage = () => {
       <section className="h-full flex flex-col justify-center items-center gap-y-24">
         <h1 className="text-6xl">MySlang</h1>
         <section className="flex flex-row gap-x-8 animate-[show_1s_ease-in]">
-          <button
-            type="button"
-            className="rounded-fill bg-[#9f9bbc] py-1.5 px-3.5 rounded-lg hover:bg-[#8c8eb8]"
-          >
-            Login
-          </button>
+          <Link to='/login'>
+            <button
+              type="button"
+              className="rounded-fill bg-[#9f9bbc] py-1.5 px-3.5 rounded-lg hover:bg-[#8c8eb8]"
+            >
+              Login
+            </button>
+          </Link>
+          <Link to='/join'>
           <button
             type="button"
             className="rounded-fill bg-[#9f9bbc] py-1.5 px-3.5 rounded-lg hover:bg-[#8c8eb8]"
           >
             Join
           </button>
+          </Link>
         </section>
       </section>
     </Wrapper>
