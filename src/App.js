d8ee13e596b2d27e13c582aa10bdf545ade414b9
Jinnie-kim <hjk7639@gmail.com>
2022-10-13 17:59:52 +0900

    Add: 프로젝트에 사용된 아이콘의 저작권을 표시하기위한 코드 추가

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ const App = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/join" element={<Join />} />
       </Routes>
+      <p className='fixed bottom-1.5 left-1/2 -translate-x-1/2 text-[8px] text-[#ababab]'>아이콘 제작자 Freepik from www.flaticon.com</p>
     </BrowserRouter>
   );
 };
