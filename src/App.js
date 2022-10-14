9e3027ae7fadd8ce3451eea858e6d73e6676d691
Jinnie-kim <hjk7639@gmail.com>
2022-10-15 01:05:06 +0900

    Feat: Home, Profile, Search 컴포넌트 라우터 생성

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import InitPage from './pages/InitPage';
 import Login from './pages/Login';
 import Join from './pages/Join';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import Search from './pages/Search';
 
 const App = () => {
   return (
@@ -10,8 +13,13 @@ const App = () => {
         <Route path="/" element={<InitPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/join" element={<Join />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/search" element={<Search />} />
       </Routes>
-      <p className='fixed bottom-1.5 left-1/2 -translate-x-1/2 text-[8px] text-[#ababab]'>아이콘 제작자 Freepik from www.flaticon.com</p>
+      <p className="fixed bottom-1.5 left-1/2 -translate-x-1/2 text-[8px] text-[#ababab]">
+        아이콘 제작자 Freepik from www.flaticon.com
+      </p>
     </BrowserRouter>
   );
 };
