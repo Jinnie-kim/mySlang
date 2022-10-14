a44aa86fa80fd85a26beb35944d53706a2504b66
Jinnie-kim <hjk7639@gmail.com>
2022-10-14 10:12:28 +0900

    Style: Wrapper 컴포넌트에 max width와 고정 height 값 수정

--- src/components/Wrapper.jsx
+++ src/components/Wrapper.jsx
@@ -1,6 +1,6 @@
 const Wrapper = (props) => {
   return (
-    <section className="container max-lg h-5/6 absolute top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4 p-5 rounded-lg overflow-hidden shadow-2xl bg-[#e2e2e2]">
+    <section className="container max-w-screen-xl h-[620px] absolute top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4 p-5 rounded-lg overflow-hidden shadow-2xl bg-[#e2e2e2]">
       {props.children}
     </section>
   );
