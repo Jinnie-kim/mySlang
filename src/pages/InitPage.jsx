b32debdb4b098d48bd30dd1a06bce5e194f3f85a
Jinnie-kim <hjk7639@gmail.com>
2022-10-15 00:58:53 +0900

    Fix: 로그인없이 이용할 수 있도록 start 버튼을 시작페이지에 추가

--- src/pages/InitPage.jsx
+++ src/pages/InitPage.jsx
@@ -7,20 +7,20 @@ const InitPage = () => {
       <section className="h-full flex flex-col justify-center items-center gap-y-24">
         <h1 className="text-6xl">MySlang</h1>
         <section className="flex flex-row gap-x-8 animate-[show_1s_ease-in]">
-          <Link to='/login'>
+          <Link to='/home'>
             <button
               type="button"
               className="rounded-fill bg-[#9f9bbc] py-1.5 px-3.5 rounded-lg hover:bg-[#8c8eb8]"
             >
-              Login
+              start
             </button>
           </Link>
-          <Link to='/join'>
+          <Link to='/login'>
           <button
             type="button"
             className="rounded-fill bg-[#9f9bbc] py-1.5 px-3.5 rounded-lg hover:bg-[#8c8eb8]"
           >
-            Join
+            login
           </button>
           </Link>
         </section>
