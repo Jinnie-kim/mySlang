f9f3399fffd71c6fa37759b955a357f62d732485
Jinnie-kim <hjk7639@gmail.com>
2022-10-16 15:11:27 +0900

    Style: Button UI 컴포넌트 생성

---
+++ src/components/Button.jsx
@@ -0,0 +1,11 @@
+const Button = (props) => {
+  return (
+    <button
+      className={`${props.width} ${props.float} rounded-fill bg-[#9f9bbc] py-1 px-3 rounded-lg hover:bg-[#8c8eb8]`}
+    >
+      {props.children}
+    </button>
+  );
+};
+
+export default Button;
