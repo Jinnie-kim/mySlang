9e494dfdb69f794e97e1fe41ee606f0e10fb90bf
Jinnie-kim <hjk7639@gmail.com>
2022-10-20 07:47:09 +0900

    Feat: global context를 사용하기 위해 useContext 커스텀 hook을 생성

---
+++ src/store/useGlobalContext.jsx
@@ -0,0 +1,10 @@
+import { useContext } from 'react';
+import { GlobalContext } from './GlobalContext';
+
+const useGlobalContext = () => {
+  const context = useContext(GlobalContext);
+  
+  return context;
+};
+
+export default useGlobalContext;
