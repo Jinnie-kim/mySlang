1a04df95256403bb74fdc654e697cd6c17a2657c
Jinnie-kim <hjk7639@gmail.com>
2022-10-19 23:55:45 +0900

    Feat: Context를 사용할 범위 지정

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css'
+import { GlobalContextProvider } from './store/GlobalContext';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <GlobalContextProvider>
+      <App />
+    </GlobalContextProvider>
   </React.StrictMode>
 );
