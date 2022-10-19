6d718a3e175389de51c69d5f3d4a264caebc64cc
Jinnie-kim <hjk7639@gmail.com>
2022-10-19 10:40:58 +0900

    Feat: Search 페이지에 단어 검색 api 붙이기, 단어 입력 & 검색 데이터를 받아오기 위해 useState 사용

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { getSearcedhWords } from '../api/word/word';
 
 import Wrapper from '../components/Wrapper';
 import Nav from '../components/Nav';
@@ -8,6 +9,16 @@ import SearchImg from '../assets/search.png';
 
 const Search = () => {
   const [openModal, setOpenModal] = useState(undefined);
+  const [word, setWord] = useState('');
+  const [wordDefs, setWordDefs] = useState([]);
+
+  const getWordHandler = (event) => {
+    setWord(event.target.value);
+  };
+
+  const getWordResult = () => {
+    getSearcedhWords(word).then((result) => setWordDefs(result));
+  };
 
   const openModalHandler = () => {
     setOpenModal({
@@ -35,13 +46,19 @@ const Search = () => {
         <Nav />
         <section className="flex flex-row items-top justify-around">
           <section className="w-[275px] text-center p-2">
-            <div className='h-9 flex'>
+            <div className="h-9 flex">
               <input
                 type="text"
                 placeholder="단어 검색하기"
                 className="rounded-lg p-1 focus-visible:outline-[#8c8eb8] mr-2"
+                value={word}
+                onChange={getWordHandler}
               />
-              <button type="submit" className="p-1.5 bg-[#f78eab] rounded-3xl">
+              <button
+                type="submit"
+                className="p-1.5 bg-[#f78eab] rounded-3xl"
+                onClick={getWordResult}
+              >
                 <img
                   src={SearchImg}
                   alt="검색 버튼 이미지"
@@ -51,82 +68,28 @@ const Search = () => {
             </div>
           </section>
           <section className="w-[828px] h-[480px] relative">
-            {/* <span className="absolute top-[50%] left-[50%] -translate-x-2/4 -translate-y-2/4 text-2xl text-[#8c8eb8]">
-            찾는 단어가 없어요!🤪
-          </span> */}
-            <ul className="w-[828px] flex flex-col gap-y-2 h-[30rem] min-h-fit p-2 overflow-y-scroll no-scrollbar">
-              <WordList
-                word="Sick"
-                definition="cool"
-                example="thats sick man"
-                author="awkward misunderstood kid"
-                onClick={openModalHandler}
-                button="add"
-                float="float-right"
-              />
-              <li className="p-2 border-2 border-[#f68eab] rounded-lg bg-[aliceblue]">
-                <h1 className="mb-5 font-bold text-2xl text-[#7376AC]">
-                  Sillybilly
-                </h1>
-                <strong className="w-3/5 inline-block mb-2 font-normal">
-                  A person, who acts in a special way and is [not afraid] to do
-                  so. Usually a small, cute, person who is very happy and can
-                  make you laugh whenever they want to. Also some one who is
-                  very fun to be around and acts in the silliest way possible.
-                </strong>
-                <p className="w-3/5 mb-2 underline">
-                  Why are you so cute, you sillybilly.
-                </p>
-                <span>author: Adafusio</span>
-                <button className="float-right rounded-fill bg-[#9f9bbc] py-1 px-3 rounded-lg hover:bg-[#8c8eb8]">
-                  add
-                </button>
-              </li>
-              <li className="p-2 border-2 border-[#f68eab] rounded-lg bg-[aliceblue]">
-                <h1 className="mb-5 font-bold text-2xl text-[#7376AC]">
-                  In the lavender haze
-                </h1>
-                <strong className="w-3/5 inline-block mb-2 font-normal">
-                  A slang term coined during the '50s, referring to being in
-                  love.
-                </strong>
-                <p className="w-3/5 mb-2 underline">
-                  Don: She really makes me laugh and smile a whole lot. Anna:
-                  Ah. You're in the lavender haze. Don: Yes... I think I'm going
-                  to ask her to marry me.
-                </p>
-                <span>author: lav3nd3r</span>
-                <button className="float-right rounded-fill bg-[#9f9bbc] py-1 px-3 rounded-lg hover:bg-[#8c8eb8]">
-                  add
-                </button>
-              </li>
-              <li className="p-2 border-2 border-[#f68eab] rounded-lg bg-[aliceblue]">
-                <h1 className="mb-5 font-bold text-2xl text-[#7376AC]">dang</h1>
-                <strong className="w-3/5 inline-block mb-2 font-normal">
-                  A euphemism for the word 'damn.'
-                </strong>
-                <p className="w-3/5 mb-2 underline">
-                  Oh dang, I just missed my train!
-                </p>
-                <span>author: Mr. X-106</span>
-                <button className="float-right rounded-fill bg-[#9f9bbc] py-1 px-3 rounded-lg hover:bg-[#8c8eb8]">
-                  add
-                </button>
-              </li>
-              <li className="p-2 border-2 border-[#f68eab] rounded-lg bg-[aliceblue]">
-                <h1 className="mb-5 font-bold text-2xl text-[#7376AC]">dang</h1>
-                <strong className="w-3/5 inline-block mb-2 font-normal">
-                  A euphemism for the word 'damn.'
-                </strong>
-                <p className="w-3/5 mb-2 underline">
-                  Oh dang, I just missed my train!
-                </p>
-                <span>author: Mr. X-106</span>
-                <button className="float-right rounded-fill bg-[#9f9bbc] py-1 px-3 rounded-lg hover:bg-[#8c8eb8]">
-                  add
-                </button>
-              </li>
-            </ul>
+            {wordDefs.length === 0 ? (
+              <span className="absolute top-[50%] left-[50%] -translate-x-2/4 -translate-y-2/4 text-2xl text-[#8c8eb8]">
+                찾는 단어가 없어요!🤪
+              </span>
+            ) : (
+              <ul className="w-[828px] flex flex-col gap-y-2 h-[30rem] min-h-fit p-2 overflow-y-scroll no-scrollbar">
+                {wordDefs.map((word) => {
+                  return (
+                    <WordList
+                      key={word.defid}
+                      word={word.word}
+                      definition={word.definition}
+                      example={word.example}
+                      author={word.author}
+                      onClick={openModalHandler}
+                      button="add"
+                      float="float-right"
+                    />
+                  );
+                })}
+              </ul>
+            )}
           </section>
         </section>
       </Wrapper>
