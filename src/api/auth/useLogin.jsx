86d2f1212481166b011db6296f7659390f24e838
Jinnie-kim <hjk7639@gmail.com>
2022-10-20 13:31:25 +0900

    Feat: firebase 로그인 커스텀 hook 생성

---
+++ src/api/auth/useLogin.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import { auth } from '../../firebase/config';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import useGlobalContext from '../../store/useGlobalContext';
+
+const useLogin = () => {
+  const [error, setError] = useState(null);
+  const [isPending, setIspending] = useState(false);
+  const { dispatch } = useGlobalContext();
+
+  const login = (email, password) => {
+    setError(null);
+    setIspending(true);
+    
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        dispatch({ type: 'login', payload: user });
+
+        setError(null);
+        setIspending(false);
+
+        if (!user) {
+          throw new Error('회원가입에 실패했습니다.');
+        }
+      })
+      .catch((error) => {
+        setError(error.message);
+        setIspending(false);
+      });
+  };
+  return { error, isPending, login };
+};
+
+export default useLogin;
