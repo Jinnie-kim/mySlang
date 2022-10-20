f2fd2314077a7ef8dff400a7e12ae944d1594855
Jinnie-kim <hjk7639@gmail.com>
2022-10-20 14:00:10 +0900

    Feat: Login 페이지에서 로그인 커스텀 hook login 사용

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
+import useLogin from '../api/auth/useLogin';
 import Wrapper from '../components/Wrapper';
 import Button from '../components/Button';
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const { login } = useLogin();
 
   const userEmailHandler = (event) => {
     setEmail(event.target.value);
@@ -16,12 +18,20 @@ const Login = () => {
     setPassword(event.target.value);
   };
 
+  const userInfoSubmitHandler = (event) => {
+    event.preventDefault();
+    login(email, password);
+  };
+
   return (
     <Wrapper>
       <section className="h-full flex flex-col justify-center items-center gap-y-60">
         <h1 className="text-6xl animate-[show_0.5s_ease-in]">MySlang</h1>
 
-        <form className="flex flex-col gap-y-5">
+        <form
+          className="flex flex-col gap-y-5"
+          onSubmit={userInfoSubmitHandler}
+        >
           <input
             type="email"
             placeholder="email"
