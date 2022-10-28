aa4baead991682d210f9d2dc479f4e4c7d87ecfc
Jinnie-kim <hjk7639@gmail.com>
2022-10-28 10:21:44 +0900

    Feat: 랜딩 페이지에서 user 정보가 있으면 start 버튼만 보여주고 user가 없으면 login 버튼도 함께 보여주도록 삼항연산자 사용

--- src/pages/InitPage.jsx
+++ src/pages/InitPage.jsx
@@ -1,28 +1,44 @@
 import { Link } from 'react-router-dom';
+import useGlobalContext from '../store/useGlobalContext';
 import Wrapper from '../components/Wrapper';
 
 const InitPage = () => {
+  const { user } = useGlobalContext();
+
   return (
     <Wrapper>
       <section className="h-full flex flex-col justify-center items-center gap-y-24">
         <h1 className="text-6xl">MySlang</h1>
         <section className="flex flex-row gap-x-8 animate-[show_1s_ease-in]">
-          <Link to='/home'>
-            <button
-              type="button"
-              className="rounded-fill bg-[#9f9bbc] py-1.5 px-3.5 rounded-lg hover:bg-[#8c8eb8]"
-            >
-              start
-            </button>
-          </Link>
-          <Link to='/login'>
-          <button
-            type="button"
-            className="rounded-fill bg-[#9f9bbc] py-1.5 px-3.5 rounded-lg hover:bg-[#8c8eb8]"
-          >
-            login
-          </button>
-          </Link>
+          {user ? (
+            <Link to="/home">
+              <button
+                type="button"
+                className="rounded-fill bg-[#9f9bbc] py-1.5 px-3.5 rounded-lg hover:bg-[#8c8eb8]"
+              >
+                start
+              </button>
+            </Link>
+          ) : (
+            <>
+              <Link to="/home">
+                <button
+                  type="button"
+                  className="rounded-fill bg-[#9f9bbc] py-1.5 px-3.5 rounded-lg hover:bg-[#8c8eb8]"
+                >
+                  start
+                </button>
+              </Link>
+              <Link to="/login">
+                <button
+                  type="button"
+                  className="rounded-fill bg-[#9f9bbc] py-1.5 px-3.5 rounded-lg hover:bg-[#8c8eb8]"
+                >
+                  login
+                </button>
+              </Link>
+            </>
+          )}
         </section>
       </section>
     </Wrapper>
