6f860dfa0160019c70a49cf31c5eee96e2ef7ac8
Jinnie-kim <hjk7639@gmail.com>
2022-10-28 10:12:10 +0900

    Style: Load UI 컴포넌트 생성

---
+++ src/components/Load.jsx
@@ -0,0 +1,13 @@
+import Wrapper from '../components/Wrapper';
+
+const Load = () => {
+  return (
+    <Wrapper>
+      <h1 className="absolute top-2/4 left-2/4 -translate-x-2/4 -translate-y-2/4">
+        로그인 정보를 확인하는 중 입니다...🧐
+      </h1>
+    </Wrapper>
+  );
+};
+
+export default Load;
